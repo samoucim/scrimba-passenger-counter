1f2b0181ee79dd7c428dd97eb45c80ea0b7296f9
Samo Ucim Web <106067752+samoucim@users.noreply.github.com>
2023-03-18 11:28:34 +0100

    Really?

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
+let countAll = document.getElementById("count-all");
 let count = 0;
+let countTotal = 0;
 
 function increment() {
   count += 1;
@@ -9,7 +11,18 @@ function increment() {
 
 function save() {
   let countStr = count + " - ";
-  saveEl.textContent += countStr;
+  saveEl.textContent += " " + countStr;
   countEl.textContent = 0;
+  countTotal += count;
   count = 0;
+  countAll.textContent = "TOTAL: " + countTotal;
+  console.log(countAll, countTotal);
+}
+
+function reset() {
+  count = 0;
+  countTotal = 0;
+  countAll.textContent = "TOTAL: 0";
+  countEl.textContent = "0";
+  saveEl.textContent = "Previous entries:";
 }
